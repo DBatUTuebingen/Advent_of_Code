9056a045a7b8a4313ef2462d732f388c155dfb8d
Louisa Lambrecht <louisa.lambrecht@uni-tuebingen.de>
2023-06-06 15:18:15 +0200

    clean up: day 13 part 1 done (relates #2)

--- 2022/13/ds1-intuition.sql
+++ 2022/13/ds1-intuition.sql
@@ -137,10 +137,9 @@ collector(i, idx, parent, id, ids, fst, snd, rec, res, finished, last_iter) AS (
             SELECT x+1, c.*
             FROM collector c, last_iteration l, to_parent p
             WHERE c.rec AND c.idx = l.idx AND c.idx = p.idx
-            AND (l.ids[x+1]+1 = c.idx OR list_contains(c.ids, l.ids[x+1]+1)) -- fixed a +1 bug here
-            AND NOT c.idx IN (SELECT idx FROM to_parent WHERE i IS NOT NULL) -- go up just to next node, not second next as well; fixed bug with "WHERE i IS NOT NULL" -> it used to always delete the result because it thought there already was one
-            -- AND NOT c.idx IN (SELECT idx FROM to_sibling_or_children)       -- this should be covered anyway by the fact that to_parent is initialized with idxs which is already filtered from to_sibling_or_children
-            -- AND l.ids[x+1] IS NOT NULL                                   -- this should be covered anyway by the fact that x=NULL doesn't hold anyway
+            AND (l.ids[x+1]+1 = c.idx OR list_contains(c.ids, l.ids[x+1]+1))
+            -- go up just to next node, not second next as well
+            AND NOT c.idx IN (SELECT idx FROM to_parent WHERE i IS NOT NULL)
             )
         )
         -- when finding the correct subtree to follow:
